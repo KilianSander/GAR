395ebf80d259b9bf9b1beeb5ee4e2f56269823a2
Kilian Sander <kilian.sander@aol.com>
2024-05-07 18:07:33 +0200

    first attempt to randomise questions per participant without using `restart.txt`

---
+++ R/radiobutton_matrix_module.R
@@ -0,0 +1,257 @@
+#' Audio N-Alternative Choice Matrix Module
+#'
+#' This function defines a module of several Audio N-Alternative Choice Matrix
+#' pages for incorporation into a \code{psychTestR} timeline.
+#' Use this function if you want to include the \code{NAC_matrix_module} in a
+#' battery of other tests, or if you want to add custom psychTestR pages to your
+#' test timeline.
+#'
+#' @export
+audio_NAC_matrix_module <- function(label = "EM01",
+                                    questionnaire = "EMO1",
+                                    response_scale = "L7",
+                                    items_prefix_pattern = "s%02d",
+                                    # item_order = NULL,
+                                    stimuli_order = NULL,
+                                    num_stimuli = 10,
+                                    num_rating_items = 6,
+                                    anchors = FALSE,
+                                    header = "double",
+                                    header_style = NULL,
+                                    reduce_labels = TRUE,
+                                    style = default_style,
+                                    allow_na = TRUE,
+                                    audio_url = "https://s3.eu-west-1.amazonaws.com/media.gold-msi.org/test_materials/GAR/EMO1",
+                                    audio_type = "wav",
+                                    dict = GAR::GAR_dict,
+                                    random_order = FALSE,
+                                    randomize_stimuli = FALSE,
+                                    show_controls = TRUE,
+                                    allow_download = FALSE,
+                                    question_header_offset = 0,
+                                    question_header_max = num_stimuli,
+                                    ...) {
+  # browser()
+  dots <- list(...)
+  MAS_IE <- FALSE
+  if("MAS_IE" %in% names(dots)){
+    MAS_IE <- dots$MAS_IE
+  }
+
+  quest <- get_questionnaires()
+  if(!(questionnaire %in% quest$id)){
+    stop(sprintf("Unknown questionnaire: %s", questionnaire))
+  }
+
+  num_rating_items <-
+    max(1, min(num_rating_items, quest[quest$id == questionnaire,]$max_items))
+  scale_length <-
+    as.numeric(stringr::str_extract(response_scale, "[0-9]+"))
+
+  preamble_key <- sprintf("TGAR_%s_PREAMBLE", questionnaire)
+  prompt_key <- sprintf("TGAR_%s_%%04d_PROMPT", questionnaire)
+  label_key <- sprintf("TGAR_%s_CHOICE%%01d", response_scale)
+
+  if(!is.null(stimuli_order)){
+    stopifnot(length(stimuli_order) == num_stimuli)
+  }
+
+  pages <- function(num_stimuli,
+                    label,
+                    audio_url,
+                    stimuli_order,
+                    items_prefix_pattern,
+                    audio_type,
+                    state,
+                    anchors,
+                    header,
+                    header_style,
+                    reduce_labels,
+                    style,
+                    num_rating_items,
+                    prompt_key,
+                    scale_length,
+                    label_key,
+                    show_controls,
+                    allow_download,
+                    allow_na,
+                    dict,
+                    ...) {
+    lapply(
+      1:num_stimuli,
+      function(id) {
+        page_label <- sprintf("%s_%02d", label, id)
+        stimulus_url <-
+          file.path(
+            audio_url,
+            sprintf(
+              "%s.%s",
+              ifelse(
+                is.null(stimuli_order),
+                sprintf(items_prefix_pattern, id),
+                stimuli_order[id]
+              ),
+              audio_type)
+          )
+        psychTestR::new_timeline(
+          psychTestR::join(
+            psychTestR::reactive_page(
+              fun = function(state, ...) {
+                matrix_item_order <- access_matrix_item_order(label = label,
+                                                              state = state)
+                audio_NAC_matrix_page(
+                  label = page_label,
+                  url = stimulus_url,
+                  instruction = psychTestR::i18n(preamble_key),
+                  anchors = anchors,
+                  header = header,
+                  header_style = header_style,
+                  sublabel_type = "directed",
+                  reduce_labels = reduce_labels,
+                  style = style,
+                  trigger_button_text = psychTestR::i18n("CONTINUE"),
+                  failed_validation_message = psychTestR::i18n("ANSWER_MISSING"),
+                  items = sapply(1:num_rating_items, function(x) psychTestR::i18n(sprintf(prompt_key, x)), simplify = T, USE.NAMES = T),
+                  choices = 0:(scale_length-1),
+                  labels = sapply(1:scale_length, function(x) psychTestR::i18n(sprintf(label_key, x)), simplify = T, USE.NAMES = T),
+                  # random_order = random_order,
+                  matrix_item_order = matrix_item_order,
+                  show_controls = show_controls,
+                  allow_download = allow_download,
+                  allow_na = allow_na
+                )
+              },
+              next_elt = TRUE
+            ),
+            psychTestR::elt_save_results_to_disk(complete = TRUE)
+          ),
+          dict = dict
+        )
+      }
+    )
+  }
+
+
+  save_stimuli <- function(label){
+    function(order, state, ...){
+      #browser()
+      if(!is.null(stimuli_order)){
+        stimuli <- sprintf("%s", stimuli_order[1:num_stimuli])[order]
+      }
+      else{
+        stimuli <- sprintf(items_prefix_pattern, 1:num_stimuli)[order]
+      }#
+      message(sprintf("Saving stimulus order for  %s (length: %d): %s",
+                      label, length(order),
+                      paste(stimuli, collapse = ", ")))
+      psychTestR::save_result(state, label, stimuli)
+    }
+  }
+
+  if (randomize_stimuli) {
+    psychTestR::join(
+      psychTestR::begin_module(label = label),
+      matrix_item_order_module(num_rating_items = num_rating_items,
+                               random_order = random_order),
+      psychTestR::randomise_at_run_time(
+        label = label,
+        logic = pages(
+          num_stimuli = num_stimuli,
+          label = label,
+          audio_url = audio_url,
+          stimuli_order = stimuli_order,
+          items_prefix_pattern = items_prefix_pattern,
+          audio_type = audio_type,
+          state = state,
+          anchors = anchors,
+          header = header,
+          header_style = header_style,
+          reduce_labels = reduce_labels,
+          style = style,
+          num_rating_items = num_rating_items,
+          prompt_key = prompt_key,
+          scale_length = scale_length,
+          label_key = label_key,
+          show_controls = show_controls,
+          allow_download = allow_download,
+          allow_na = allow_na,
+          dict = dict,
+          ...
+        ),
+        save_order = save_stimuli(sprintf("%s_stimulus_order", label))
+      ),
+      psychTestR::end_module()
+    )
+  } else {
+    psychTestR::join(
+      psychTestR::begin_module(label = label),
+      matrix_item_order_module(num_rating_items = num_rating_items,
+                               random_order = random_order),
+      psychTestR::order_at_run_time(
+        label = label,
+        logic = pages(
+          num_stimuli = num_stimuli,
+          label = label,
+          audio_url = audio_url,
+          stimuli_order = stimuli_order,
+          items_prefix_pattern = items_prefix_pattern,
+          audio_type = audio_type,
+          state = state,
+          anchors = anchors,
+          header = header,
+          header_style = header_style,
+          reduce_labels = reduce_labels,
+          style = style,
+          num_rating_items = num_rating_items,
+          prompt_key = prompt_key,
+          scale_length = scale_length,
+          label_key = label_key,
+          show_controls = show_controls,
+          allow_download = allow_download,
+          allow_na = allow_na,
+          dict = dict,
+          ...
+        ),
+        get_order = function(...) 1:num_stimuli,
+        save_order = save_stimuli(sprintf("%s_stimulus_order", label))
+      ),
+      psychTestR::end_module()
+    )
+  }
+}
+
+matrix_item_order_module <- function(num_rating_items,
+                                     random_order = FALSE,
+                                     label = "matrix_item_order") {
+  stopifnot(num_rating_items > 0,
+            is.logical(random_order))
+
+  psychTestR::join(
+    psychTestR::begin_module(label = label),
+    psychTestR::code_block(
+      function(state, ...) {
+        if (random_order) {
+          order <- sample(num_rating_items)
+        } else {
+          order <- 1:num_rating_items
+        }
+        psychTestR::save_result(
+          place = state,
+          label = "order",
+          value = order
+        )
+      }
+    ),
+    psychTestR::elt_save_results_to_disk(complete = TRUE),
+    psychTestR::end_module()
+  )
+}
+
+access_matrix_item_order <- function(label,
+                                     sublabel = "matrix_item_order",
+                                     subsublabel = "order",
+                                     state) {
+  res <-
+    psychTestR::get_results(state = state, complete = FALSE)
+  res[[paste(label, sublabel, sep = ".")]][[subsublabel]]
+}
---
+++ R/radiobutton_matrix_pages.R
@@ -0,0 +1,360 @@
+#' N-Alternative Choice Matrix Pages
+#'
+#' The \code{*_NAC_matrix_page} functions create a radio button matrix page.
+#' They can be thought of as extensions of the
+#' \code{\link[psychTestR]{NAFC_page()}} as they not only display one question
+#' or item, but a list of questions or items to be answered on the same
+#' response scale.
+#' @name radiobutton_matrix_pages
+NULL
+
+#' @rdname radiobutton_matrix_pages
+#' @export
+NAC_matrix_page <- function(label,
+                            polarity = c("unipolar", "bipolar"),
+                            items,
+                            choices,
+                            instruction = NULL,
+                            labels = NULL,
+                            anchors = FALSE,
+                            header = c("double", "simple_str", "simple_num"),
+                            header_style = NULL,
+                            sublabel_type = c("none", "directed", "symmetric"),
+                            reduce_labels = TRUE,
+                            style = default_style,
+                            trigger_button_text = "Continue",
+                            allow_na = FALSE,
+                            failed_validation_message = "Answer missing!",
+                            save_answer = TRUE,
+                            hide_response_ui = FALSE,
+                            # random_order = FALSE,
+                            matrix_item_order = 1:length(items),
+                            response_ui_id = "response_ui",
+                            on_complete = NULL,
+                            admin_ui = NULL) {
+  stopifnot(
+    is.scalar.character(label),
+    is.character.vector(items),
+    is.scalar.character(trigger_button_text),
+    is.scalar.character(failed_validation_message),
+    is.character.or.numeric(choices),
+    length(choices) > 0L
+  )
+
+  sublabel_type <- match.arg(sublabel_type)
+
+  instruction_tag <- NULL
+
+  if(!is.null(instruction)) {
+    instruction_tag <- tagify(instruction)
+  }
+
+  items <- items[matrix_item_order]
+
+  ui <- shiny::tags$div(
+    instruction_tag,
+    make_ui_radiobutton_matrix(
+      label,
+      polarity = polarity,
+      items = items,
+      scale_labels = labels,
+      choices,
+      style = style,
+      trigger_button_text = trigger_button_text,
+      anchors = anchors,
+      header = header,
+      header_style = header_style,
+      sublabel_type = sublabel_type,
+      reduce_labels = reduce_labels,
+      hide = hide_response_ui,
+      id = response_ui_id
+    )
+  )
+
+  get_answer <- function(input, ...) {
+    values <-shiny::reactiveValuesToList(input)$radio_matrix
+    answer <-
+      purrr::map(
+        values,
+        function(x) {
+          if(!is.null(x[[1]])) {
+            as.numeric(x[[1]])
+          } else NA
+        }
+      ) %>%
+      unlist()
+
+    names(answer) <- sprintf("%s.q%d", label, matrix_item_order)
+    answer
+  }
+
+  validate <- function(answer,  ...) {
+    valid  <- TRUE
+    #messagef("[validate] allow_na: %s", allow_na)
+    if(is.logical(allow_na)){
+      if(!allow_na){
+        if(any(is.na(answer))){
+          valid <- failed_validation_message
+        }
+      }
+    }
+    else{
+      na_count <- sum(is.na(answer))
+      if(na_count > allow_na){
+        valid <- failed_validation_message
+      }
+    }
+    valid
+  }
+
+  psychTestR::page(
+    ui = ui,
+    label = label,
+    get_answer = get_answer,
+    save_answer = save_answer,
+    validate = validate,
+    on_complete = on_complete,
+    final = FALSE,
+    admin_ui = admin_ui
+  )
+}
+
+#' @rdname radiobutton_matrix_pages
+#' @export
+audio_NAC_matrix_page <- function(label,
+                                  polarity = c("unipolar", "bipolar"),
+                                  items,
+                                  choices,
+                                  url,
+                                  instruction = "",
+                                  labels = NULL,
+                                  anchors = FALSE,
+                                  header = "double",
+                                  header_style = NULL,
+                                  sublabel_type = c("none", "directed", "symmetric"),
+                                  style = default_style,
+                                  reduce_labels = TRUE,
+                                  trigger_button_text = "Continue",
+                                  allow_na = FALSE,
+                                  failed_validation_message = "Answer missing!",
+                                  save_answer = TRUE,
+                                  hide_response_ui = FALSE,
+                                  # random_order = FALSE,
+                                  matrix_item_order = 1:length(items),
+                                  response_ui_id = "response_ui",
+                                  on_complete = NULL,
+                                  audio_type = tools::file_ext(url),
+                                  wait = TRUE,
+                                  loop = FALSE,
+                                  admin_ui = NULL,
+                                  btn_play_prompt = if (!show_controls) "Click here to play",
+                                  show_controls = FALSE,
+                                  allow_download = FALSE) {
+  stopifnot(
+    is.scalar.character(label),
+    is.character.or.numeric(choices),
+    is.scalar.character(url),
+    is.scalar.logical(wait),
+    is.scalar.logical(loop),
+    is.scalar.logical(show_controls),
+    is.scalar.logical(allow_download),
+    is.scalar.logical(hide_response_ui)
+  )
+
+  audio_ui <- shiny::tags$div(shiny::tags$audio(
+    shiny::tags$head(shiny::tags$script(shiny::HTML(media.js$media_not_played))),
+    shiny::tags$source(src = url, type = paste0("audio/", audio_type)),
+    id = "media",
+    preload = "auto",
+    autoplay = "autoplay",
+    loop = if (loop) "loop",
+    oncanplaythrough = media.js$show_media_btn,
+    onplay = paste0(media.js$media_played, media.js$hide_media_btn),
+    onended = if (wait) media.js$show_responses else "null",
+    controls = if (show_controls) "controls",
+    controlsList = if (!allow_download) "nodownload"
+  ), media_mobile_play_button(btn_play_prompt))
+  instruction2 <- shiny::tags$div(tagify(instruction),
+                                  #shiny::span(url, style = "color:red"),
+                                  audio_ui)
+
+  NAC_matrix_page(
+    label = label,
+    polarity = polarity,
+    items = items,
+    choices = choices,
+    instruction = instruction2,
+    labels = labels,
+    anchors = anchors,
+    header = header,
+    header_style = header_style,
+    sublabel_type = sublabel_type,
+    reduce_labels = reduce_labels,
+    style = style,
+    trigger_button_text = trigger_button_text,
+    allow_na = allow_na,
+    failed_validation_message = failed_validation_message,
+    save_answer = save_answer,
+    hide_response_ui = hide_response_ui,
+    matrix_item_order = matrix_item_order,
+    response_ui_id = response_ui_id,
+    on_complete = on_complete,
+    admin_ui = admin_ui
+  )
+}
+
+#' @rdname radiobutton_matrix_pages
+video_NAC_matrix_page <- function(label,
+                                  polarity = c("unipolar", "bipolar"),
+                                  items,
+                                  choices,
+                                  url,
+                                  instruction = "",
+                                  labels = NULL,
+                                  anchors = FALSE,
+                                  header = "double",
+                                  header_style = NULL,
+                                  sublabel_type = c("none", "directed", "symmetric"),
+                                  style = default_style,
+                                  reduce_labels = TRUE,
+                                  trigger_button_text = "Continue",
+                                  allow_na = FALSE,
+                                  failed_validation_message = "Answer missing!",
+                                  save_answer = TRUE,
+                                  hide_response_ui = FALSE,
+                                  # random_order = FALSE,
+                                  matrix_item_order = 1:length(items),
+                                  response_ui_id = "response_ui",
+                                  on_complete = NULL,
+                                  video_type = tools::file_ext(url),
+                                  video_width = "100%",
+                                  wait = TRUE,
+                                  loop = FALSE,
+                                  admin_ui = NULL,
+                                  btn_play_prompt = if (!show_controls) "Click here to play",
+                                  show_controls = FALSE,
+                                  allow_download = FALSE) {
+  stopifnot(
+    is.scalar.character(label),
+    is.character.or.numeric(choices),
+    is.scalar.character(url),
+    is.scalar.logical(wait),
+    is.scalar.logical(loop),
+    is.scalar.logical(show_controls),
+    is.scalar.logical(allow_download),
+    is.scalar.logical(hide_response_ui)
+  )
+
+  video_ui <-
+    shiny::tags$div(
+      shiny::tags$video(
+        shiny::tags$head(shiny::tags$script(shiny::HTML(media.js$media_not_played))),
+        shiny::tags$source(src = url, type = paste0("video/", video_type)),
+        id = "media", width = video_width, preload = "auto",
+        autoplay = "autoplay", style = "max-width: 500px",
+        playsinline = "playsinline",
+        loop = if (loop) "loop",
+        oncanplaythrough = media.js$show_media_btn,
+        onplay = paste0(media.js$media_played, media.js$hide_media_btn),
+        onended = if (wait) media.js$show_responses else "null",
+        controls = if (show_controls) "controls",
+        controlsList = if (!allow_download) "nodownload",
+        disablePictureInPicture = "disablePictureInPicture"
+      ),
+      media_mobile_play_button(btn_play_prompt)
+    )
+
+  instruction2 <- shiny::tags$div(tagify(instruction),
+                                  video_ui)
+
+  NAC_matrix_page(
+    label = label,
+    polarity = polarity,
+    items = items,
+    choices = choices,
+    instruction = instruction2,
+    labels = labels,
+    anchors = anchors,
+    header = header,
+    header_style = header_style,
+    sublabel_type = sublabel_type,
+    reduce_labels = reduce_labels,
+    style = style,
+    trigger_button_text = trigger_button_text,
+    allow_na = allow_na,
+    failed_validation_message = failed_validation_message,
+    save_answer = save_answer,
+    hide_response_ui = hide_response_ui,
+    matrix_item_order = matrix_item_order,
+    response_ui_id = response_ui_id,
+    on_complete = on_complete,
+    admin_ui = admin_ui
+  )
+}
+
+#' @rdname radiobutton_matrix_pages
+image_NAC_matrix_page <- function(label,
+                                  polarity = c("unipolar", "bipolar"),
+                                  items,
+                                  choices,
+                                  url,
+                                  img_width = "100%",
+                                  instruction = NULL,
+                                  labels = NULL,
+                                  anchors = FALSE,
+                                  header = c("double", "simple_str", "simple_num"),
+                                  header_style = NULL,
+                                  sublabel_type = c("none", "directed", "symmetric"),
+                                  reduce_labels = TRUE,
+                                  style = default_style,
+                                  trigger_button_text = "Continue",
+                                  allow_na = FALSE,
+                                  failed_validation_message = "Answer missing!",
+                                  save_answer = TRUE,
+                                  hide_response_ui = FALSE,
+                                  # random_order = FALSE,
+                                  matrix_item_order = 1:length(items),
+                                  response_ui_id = "response_ui",
+                                  on_complete = NULL,
+                                  admin_ui = NULL) {
+  stopifnot(
+    is.scalar.character(label),
+    is.character.or.numeric(choices),
+    is.scalar.character(url)
+  )
+
+  # PRELIMINARY!
+  # img_width should automatically be adjusted to the given layout
+  instruction2 <-
+    shiny::tags$div(
+      tagify(instruction),
+      shiny::tags$img(
+        width = img_width,
+        src = url
+      )
+    )
+
+  NAC_matrix_page(
+    label = label,
+    polarity = polarity,
+    items = items,
+    choices = choices,
+    instruction = instruction2,
+    labels = labels,
+    anchors = anchors,
+    header = header,
+    header_style = header_style,
+    sublabel_type = sublabel_type,
+    reduce_labels = reduce_labels,
+    style = style,
+    trigger_button_text = trigger_button_text,
+    allow_na = allow_na,
+    failed_validation_message = failed_validation_message,
+    save_answer = save_answer,
+    hide_response_ui = hide_response_ui,
+    matrix_item_order = matrix_item_order,
+    response_ui_id = response_ui_id,
+    on_complete = on_complete,
+    admin_ui = admin_ui
+  )
+}
